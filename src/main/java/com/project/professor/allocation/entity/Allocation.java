e778274025280cd19c5a7cb05c6a81119c6496f5
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2024-03-10 14:25:46 -0300

    add entities

---
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -0,0 +1,106 @@
+package com.project.professor.allocation.entity;
+
+import java.sql.Time;
+import java.time.DayOfWeek;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Allocation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "day", nullable = false)
+	private DayOfWeek dayOfWeek;
+
+	@Column(name = "startHour", nullable = false)
+	private Time startHour;
+
+	@Column(name = "endHour", nullable = false)
+	private Time endHour;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "professor_id", nullable = false)
+	private Professor professor;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "course_id", nullable = false)
+	private Course course;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DayOfWeek getDayOfWeek() {
+		return dayOfWeek;
+	}
+
+	public void setDayOfWeek(DayOfWeek dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+
+	public Time getStartHour() {
+		return startHour;
+	}
+
+	public void setStartHour(Time startHour) {
+		this.startHour = startHour;
+	}
+
+	public Time getEndHour() {
+		return endHour;
+	}
+
+	public void setEndHour(Time endHour) {
+		this.endHour = endHour;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	public void setProfessorId(Long id) {
+		Professor professor = new Professor();
+		professor.setId(id);
+		this.setProfessor(professor);
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public void setCourseId(Long id) {
+		Course course = new Course();
+		course.setId(id);
+		this.setCourse(course);
+	}
+
+	@Override
+	public String toString() {
+		return "Allocation{" + "id=" + id + ", dayOfWeek=" + dayOfWeek + ", startHour=" + startHour + ", endHour="
+				+ endHour + ", professor=" + professor + ", course=" + course + '}';
+	}
+}
---
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -0,0 +1,39 @@
+package com.project.professor.allocation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", unique = true, nullable = false)
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Department{" + "id=" + id + ", name='" + name + '\'' + '}';
+	}
+}
